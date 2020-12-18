b6e5010168476a04ddbab7a6068aff9eba00c0bc
BuRny <gamingburny@gmail.com>
2020-12-18 19:30:19 +0100

    Add tasks to README

---
+++ src/components/GroupInfoJoined.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class GroupInfoJoined extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/components/StudyProjectInfo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class StudyProjectInfo extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
