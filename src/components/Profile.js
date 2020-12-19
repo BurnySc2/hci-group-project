c3e9efe90798419aaf21365410b5ab1d2fedb987
DR12500 <domrit1205@yahoo.com>
2020-12-19 15:39:44 +0100

    add basic variables

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -13,6 +13,28 @@ export default class Profile extends Component {
     // pw aendern / vergessen
     // account loeschen
     render() {
-        return <div>irgendwas</div>
+        let profileInfo = {
+            "name":"Björk",
+            "degreecourse":"Informatik",
+            "semester":"5",
+            "connections":"Email, Discord, Teams",
+            "moduls":"HCI, Analysis A, Ausgewählte Kapitel des Rechts für Informatiker",
+            "description":"Außerdem bin ich sehr engagiert.",
+            "hobbies":"Meine Hobbys sind Fahrrad fahren und Kochen."
+        }
+        let mycss = "m-auto"
+        let containercss = "flex flex-col bg-gray-500"
+        return (
+            <div className={containercss}>
+                <div className={mycss}>Name: {profileInfo.name}</div>
+                <div className={mycss}>{profileInfo.degreecourse}, {profileInfo.semester}. Semester</div>
+                <div className={mycss}>Erreichbar unter: {profileInfo.connections}</div>
+                <div className={mycss}>
+                    Ich belege dieses Semester folgende Module:{profileInfo.moduls}
+                </div>
+                <div className={mycss}>{profileInfo.description}</div>
+                <div className={mycss}>{profileInfo.hobbies}</div>
+            </div>
+        )
     }
 }
