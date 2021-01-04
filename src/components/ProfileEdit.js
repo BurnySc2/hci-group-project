83be8240ef355e0807dc83c7a8fcde30e84effe2
DR12500 <domrit1205@yahoo.com>
2021-01-04 22:14:52 +0100

    added further styling features

--- src/components/ProfileEdit.js
+++ src/components/ProfileEdit.js
@@ -21,7 +21,7 @@ export default function ProfileEdit(props) {
     let myInput = (myKey, type = "text") => {
         return (
             <input
-                className="border-2"
+                className="mx-2 border-2"
                 type={type}
                 value={profileInfoChanged[myKey]}
                 onChange={(e) => {
@@ -34,6 +34,8 @@ export default function ProfileEdit(props) {
         )
     }
 
+    let labelClasses = "font-bold"
+
     return (
         <div className="m-2 p-2 flex flex-col">
             <div className="flex flex-col">
@@ -42,36 +44,37 @@ export default function ProfileEdit(props) {
                     alt={"profile pic"}
                     src={profileInfoChanged.profilePicLink}
                 />
-                <div>Profil Bild:</div>
+                <div className={labelClasses}>Profil Bild:</div>
                 {myInput("profilePicLink", "text")}{" "}
             </div>
             <div className="flex flex-col">
-                <div>Name:</div>
+                <div className={labelClasses}>Name:</div>
                 {myInput("name")}
             </div>
             <div className="flex flex-col">
-                <div>Degreecourse:</div>
+                <div className={labelClasses}>Degreecourse:</div>
                 {myInput("degreecourse")}
             </div>
             <div className="flex flex-col">
-                <div>Semester:</div>
+                <div className={labelClasses}>Semester:</div>
                 {myInput("semester", "number")}
             </div>
             <div className="flex flex-col">
-                <div>Connections:</div>
+                <div className={labelClasses}>Connections:</div>
                 {myInput("connections")}
             </div>
             <div className="flex flex-col">
-                <div>Moduls:</div>
+                <div className={labelClasses}>Moduls:</div>
                 {myInput("moduls")}
             </div>
             <div className="flex flex-col">
-                <div>Description:</div>
+                <div className={labelClasses}>Description:</div>
                 {myInput("description")}
             </div>
             {/*TODO remaining profile fields*/}
             <div className="m-2 p-2 flex justify-between">
                 <button
+                    className="font-bold text-2xl hover:bg-red-400"
                     onClick={(e) => {
                         props.setProfileShowEditScreen(false)
                     }}
@@ -79,6 +82,7 @@ export default function ProfileEdit(props) {
                     Cancel
                 </button>
                 <button
+                    className="font-bold text-2xl hover:bg-green-400"
                     onClick={(e) => {
                         storeNewProfileInfoInDatabase(
                             userName,
