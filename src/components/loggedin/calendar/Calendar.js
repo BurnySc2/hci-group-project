09df15ea588921077b11a4c1a20d76cdda9d650f
BuRny <gamingburny@gmail.com>
2021-01-08 20:36:03 +0100

    Fix CI build process

--- src/components/loggedin/calendar/Calendar.js
+++ src/components/loggedin/calendar/Calendar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import WeekView from "./WeekView"
 import MonthView from "./MonthView"
 import { BUTTONS } from "../../../css/classes"
